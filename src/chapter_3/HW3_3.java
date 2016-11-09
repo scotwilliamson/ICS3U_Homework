4cb0aaca6bbabcaa74fef87fd86c685893d51a87
cameronbray11 <cameronbray@macbook-pro.crescentschool.local>
2016-11-09 11:30:44 -0500

    3.3

--- src/chapter_3/HW3_3.java
+++ src/chapter_3/HW3_3.java
@@ -3,8 +3,17 @@ package chapter_3;
 public class HW3_3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// If statements are the main thing in this section, I already know the syntax but a few notes
+		
+		int i = 3;
+		int s = 2;
+		
+		if (i > s) {// If (compairing to things is true)
+			;//do something
+		}else// if its not true
+		{
+			;//do something else
+		}
 	}
 
 }
