91c0359795d2a2afc0b1950794429aba3640444e
cameronbray11 <cameronbray@macbook-pro.crescentschool.local>
2016-11-09 11:26:49 -0500

    3.1

--- src/chapter_3/HW3_1.java
+++ src/chapter_3/HW3_1.java
@@ -3,7 +3,29 @@ package chapter_3;
 public class HW3_1 {
 
 	public static void main(String[] args) {
-		
+//		1. For each legal expression, state its value. For each illegal expression,
+//		state the reason that it is illegal.
+//		(a) (2 + (-5)) != 3	True
+//		(b) ’m’ < = ’p’		True
+//		(c) ’Q’ == ’q’ 		False
+//		(d) ’*’ < ’*’		Error
+//		(e) 8.23 =< 8.2300 	True
+//		(f) (7 / 3) = 2		True
+//		(g) false == 0 		Error
+//		(h) (25 % 4) >= 1	True
+
+//		2.
+//		class BooleanVariables
+//		{
+//		public static void main (String[] args)
+//		{
+//		boolean perhaps, maybe;
+//		perhaps = 4 < 5;
+//		maybe = -17 % 4 == 1;
+//		System.out.println("perhaps: " + perhaps);
+//		System.out.println("maybe: " + maybe);
+//		}
+//		}
 	}
 
 }
