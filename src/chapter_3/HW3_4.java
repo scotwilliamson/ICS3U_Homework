c83d0d3f968f9e135dbb5ae3efb66ca175a3213e
cameronbray11 <cameronbray@macbook-pro.crescentschool.local>
2016-11-09 12:25:57 -0500

    3.4 Part

---
+++ src/chapter_3/HW3_4.java
@@ -0,0 +1,44 @@
+package chapter_3;
+
+public class HW3_4 {
+
+	public static void main(String[] args) {
+		//I have only done the more challenging questions
+		
+		
+//		2. Simplify as much as possible.
+//		(a) !(a <= b) 
+//		(b) !(x != 0)
+//		(c) x < 5 && x < 8 
+//		(d) a != 0 || a != 2
+//		(e) a > b || a == b 
+//		(f) x > 0 && x <= -2
+	
+	//		*Answers*	
+		
+		//a. (a>b)
+		//b. (x==0)
+		//c. x<8
+		//d. ?
+		//e. a => b
+		//f. x => -2
+		
+//		4. Use de Morgan’s laws and the fact that !!p is equivalent to p to
+//		simplify each expression.
+//		(a) !(p && !q) 
+//		(b) !(!p && !q)
+//		(c) (!(!p || !q)) 
+//		(d) !(x != 0 && !(y == 0))
+		
+	
+	//		*Answers*
+		
+		//a. !p && q
+		//b. p && q
+		//c. p && q
+		//d. x == 0 && y == 0
+		
+
+	}
+
+}
